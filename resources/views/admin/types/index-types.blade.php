a4030e9d381a00ea429a861390a595a1275142a6
alex02r <alex.giochi02@gmail.com>
2024-02-27 17:25:29 +0100

    [UPGRADE] Visualizzazione di tutti i type

--- resources/views/admin/types/index-types.blade.php
+++ resources/views/admin/types/index-types.blade.php
@@ -1,10 +1,37 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-12">
-                <h1>types</h1>
+    <div class="container">
+        <div class="row row-gap-4">
+            <div class="col-12 p-3">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="">
+                        <h1>TYPES</h1>
+                    </div>
+                    <div class="">
+                        <a class="btn btn-warning" href="{{ route('admin.types.create') }}">Add Type</a>
+                    </div>
+                </div>
             </div>
+            <table class="table table-dark">
+                <thead>
+                    <th>Nome</th>
+                    <th>Progetti associati</th>
+                    <th>Strumenti</th>
+                </thead>
+                <tbody>
+                    @foreach ($types as $type)
+                        <tr>
+                            <td>{{ $type->name }}</td>
+                            <td></td>
+                            <td>
+                                <a href="" class="btn btn-sm btn-primary">Show</a>
+                                <a href="" class="btn btn-sm btn-warning">Edit</a>
+                                <a href="" class="btn btn-sm btn-danger">Elimina</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
