f176569211c4e1772bff4624f3a22baab55be085
alex02r <alex.giochi02@gmail.com>
2024-02-27 15:46:05 +0100

    [UPGRADE] Compilazione del seeder per la crazione di nuovi record tramite faker

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +15,13 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 5; $i++) { 
+           $new_type = new Type();
+           $new_type->name = $faker->words(2, true);
+           $new_type->slug = Str::slug($new_type->name, '-');
+           $new_type->save();
+        }
     }
 }
