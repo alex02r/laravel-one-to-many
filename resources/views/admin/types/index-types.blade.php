45ba53783cb9500c22874358e336c1b321954e32
alex02r <alex.giochi02@gmail.com>
2024-02-27 17:10:20 +0100

    [UPGREADE] Creata la pagina per la visualizzazione dei type

---
+++ resources/views/admin/types/index-types.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                <h1>types</h1>
+            </div>
+        </div>
+    </div>
+@endsection
