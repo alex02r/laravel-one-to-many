00b74f9d73e97f3c87bfb3dcb1210b56ffcd1732
alex02r <alex.giochi02@gmail.com>
2024-02-27 15:29:28 +0100

    [UPGRADE] Definizione metofo down() per in caso, eliminare la colonna

--- database/migrations/2024_02_27_142454__add_type_id_foreign_key_to_projects_table.php
+++ database/migrations/2024_02_27_142454__add_type_id_foreign_key_to_projects_table.php
@@ -29,7 +29,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
